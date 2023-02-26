6fb68d97cd18daa5fa10e832cd63d017575f47d6
kevalpatel2106 <kevalpatel2106@gmail.com>
2023-02-26 17:26:07 +0000

    - Restructuring project

---
+++ settings.gradle.kts
@@ -0,0 +1,50 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+plugins {
+    // Version catalog is not supported here.
+    id("com.gradle.enterprise") version "3.12.3"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        exclusiveContent {
+            forRepository {
+                google()
+            }
+            filter {
+                includeGroupByRegex("androidx.*")
+            }
+        }
+    }
+}
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI").toBoolean()) {
+            publishAlways()
+            tag("CI")
+        }
+    }
+}
+
+rootProject.name = "Pocket CI"
+rootDir.walk().maxDepth(2).filter {
+    val notBuildSrc = it.name != "buildSrc"
+    val notTemplateDir = !it.name.contains("template")
+    val containsBuildFile = File(it, "build.gradle").exists() || File(it, "build.gradle.kts").exists()
+    notBuildSrc && notTemplateDir && containsBuildFile
+}.forEach {
+    if (it.parentFile == rootDir) {
+        include(it.name)
+    } else {
+        include("${it.parentFile.name}:${it.name}")
+    }
+}
