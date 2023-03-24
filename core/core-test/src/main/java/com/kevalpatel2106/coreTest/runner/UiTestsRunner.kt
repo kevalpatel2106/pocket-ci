5349da574d711679445ada33b53833d95f765ffc
kevalpatel2106 <kevalpatel2106@gmail.com>
2023-03-24 07:51:45 +0000

    Dismissing ANR and system dialogs before running the UI tests

--- core/core-test/src/main/java/com/kevalpatel2106/coreTest/runner/UiTestsRunner.kt
+++ core/core-test/src/main/java/com/kevalpatel2106/coreTest/runner/UiTestsRunner.kt
@@ -3,7 +3,11 @@ package com.kevalpatel2106.coreTest.runner
 import android.app.Application
 import android.app.Instrumentation
 import android.content.Context
+import android.content.Intent
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.runner.AndroidJUnitRunner
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
 import dagger.hilt.android.testing.HiltTestApplication
 
 class UiTestsRunner : AndroidJUnitRunner() {
@@ -14,5 +18,26 @@ class UiTestsRunner : AndroidJUnitRunner() {
         context: Context?,
     ): Application? {
         return Instrumentation.newApplication(HiltTestApplication::class.java, context)
+    }
+
+    override fun callApplicationOnCreate(app: Application?) {
+        super.callApplicationOnCreate(app)
+        app?.let {
+            dismissSystemDialog(it)
+            dismissANRSystemDialog()
+        }
+    }
+
+    private fun dismissSystemDialog(context: Context) {
+        context.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
+    }
+
+    private fun dismissANRSystemDialog() {
+        val device = UiDevice.getInstance(getInstrumentation())
+        // If running the device in English Locale
+        val waitButton = device.findObject(UiSelector().textContains("wait"))
+        if (waitButton.exists()) {
+            waitButton.click()
+        }
     }
 }
